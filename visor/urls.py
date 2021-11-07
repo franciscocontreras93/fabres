45d608236ce3059d4574172780c5e67f973daf5d
[Francisco Contreras] <franciscocontreras93@gmail.com>
2021-11-07 13:38:57 -0400

    update

--- visor/urls.py
+++ visor/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('', views.re_distritos, name='index'),
+    path('', views.index, name='index'),
     url('rest/fabres$', views.re_distritos, name='api'), # * EN DESARROLLO
     url('rest/query$', views.re_query, name='api'),
     url(r'rest/fabres/?distrito=(?P<distrito>[a-zA-z]+)$', views.re_distritos, name='distrito'),
