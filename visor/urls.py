27d13cf717719967aafee67a265b79449a6bb57e
[Francisco Contreras] <franciscocontreras93@gmail.com>
2021-11-08 14:59:26 -0400

    update landingpage

--- visor/urls.py
+++ visor/urls.py
@@ -7,7 +7,7 @@ from visor import views,apiviews
 urlpatterns = [
     path('', views.index, name='index'),
     re_path('rest/fabres$', apiviews.re_distritos, name='api'), 
-    re_path(r'rest/query/?(?P<width>[0-9]+)$', apiviews.re_query, name='api'),
+    re_path(r'rest/processing/buffer/?(?P<width>[0-9]+)$', apiviews.re_query, name='api'),
     re_path(r'rest/fabres/(?P<distrito>[a-zA-z]+)$', apiviews.re_distritos, name='distrito'),
     re_path(r'rest/fabres/(?P<nom_ccpp>[a-zA-z\s]+)$', apiviews.re_ccpp, name='centro poblado'),
     re_path(r'rest/fabres/(?P<idmanzana>[a-zA-z0-9]+)$', apiviews.re_manzana, name='manzana'),
