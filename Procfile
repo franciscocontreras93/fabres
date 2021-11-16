77ae56e2538f50dd0f44f94116de74bcd4725296
[Francisco Contreras] <franciscocontreras93@gmail.com>
2021-11-16 14:21:21 -0400

    Remove ignored data from gitignore

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn fabres.wsgi:application --log-file -
--- run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo activating pyenv
-source ../../fabres-env/scripts/activate
-echo pyenv activated
-echo runing django server
-py manage.py runserver
