c0ddb41c1c2c979cd70143861ec15c3eec452131
[Francisco Contreras] <franciscocontreras93@gmail.com>
2021-11-23 18:10:14 -0400

    update

--- visor/views.py
+++ visor/views.py
@@ -24,13 +24,13 @@ def SumCount(dq, propertie, value, operation=''):
         total = dq.filter(n_riesgo=value).aggregate(t=Count('pob_total'))
         op = total['t']
         return op
-    if propertie == 'q_densidad' and operation == 'Sum':
-        total = dq.filter(q_densidad=value).aggregate(t=Sum('pob_total'))
+    if propertie == 'q_densid' and operation == 'Sum':
+        total = dq.filter(q_densid=value).aggregate(t=Sum('pob_total'))
         op = total['t']
         return op
 
-    if propertie == 'q_densidad' and operation == 'Count':
-        total = dq.filter(q_densidad=value).aggregate(t=Count('pob_total'))
+    if propertie == 'q_densid' and operation == 'Count':
+        total = dq.filter(q_densid=value).aggregate(t=Count('pob_total'))
         op = total['t']
         return op
     if propertie == 'q_propnbi' and operation == 'Sum':
@@ -61,6 +61,36 @@ def pobTotal(dq):
     return op
 
 
+def getIndex(clases,dq,variable, colores=[]):
+    
+    clases = clases
+    poblacion = []
+    porcentaje = []
+    manzanas = []
+    colores = colores
+    
+
+    
+
+    for x in clases:
+        poblacion.append(SumCount(dq, variable, x, 'Sum'))
+    for x in clases:
+        manzanas.append(SumCount(dq, variable, x, 'Count'))
+    for i in poblacion:
+        porcentaje.append(round((i*100/pobTotal(dq)), 2))
+
+    tabla = list(zip(clases, manzanas, poblacion, porcentaje, colores))
+    
+
+    mapIndex = {
+        'tabla': tabla,
+        'total_manzanas': sum(manzanas),
+        'total_porcentaje': round(sum(porcentaje),0)
+    }
+
+    print(variable, mapIndex['tabla'])
+    
+    return mapIndex
 
 
 
@@ -75,6 +105,12 @@ def index(request):
 
     })
 
+
+
+
+
+
+
 @login_required
 def getDistrito(request): 
     query = request.GET.get('search')
@@ -85,78 +121,88 @@ def getDistrito(request):
     })
 
 
+
+
 @login_required
 def webmap(request):
     query = request.GET.get('search')
     dq = model.objects.all()
     nombre = ''
-    class_riesgo = ['Muy Alto', 'Alto', 'Medio', 'Bajo']
-    colores_riesgo = ["#dc3545c2", "#ff6a00c2", "#ffc107c2","#198754c2"]
-    pob_riesgo = []
-    qm_riesgo = []
-    percent_pob = []
     total = pobTotal(dq)
     data = FS.PublicSerializer(dq)
-    
-    for x in class_riesgo:
-        pob_riesgo.append(SumCount(dq, 'n_riesgo', x, 'Sum'))
-    for x in class_riesgo:
-        qm_riesgo.append(SumCount(dq, 'n_riesgo', x, 'Count'))
-    for i in pob_riesgo:
-        percent_pob.append(round((i*100/total), 2))
-    
+    data_riesgo = getIndex(['Muy Alto', 'Alto', 'Medio', 'Bajo'], dq, 'n_riesgo', ["#dc3545c2", "#ff6a00c2", "#ffc107c2", "#198754c2", ])
+    data_densidad = getIndex(['Q1', 'Q2', 'Q3', 'Q4', 'Q5'], dq, 'q_densid', [
+                             '#fde725c2', '#5dc962c2', '#20908dc2', '#3a528bc2', '#440154c2'])
+
+
 
     if query != None:
         dq = dq.filter(
             Q(distrito__icontains=query) | Q(nom_ccpp__icontains=query))
         data = FS.PublicSerializer(dq)
         total = pobTotal(dq)
-        pob_riesgo = []
-        qm_riesgo = []
-        percent_pob = []
-        for x in class_riesgo: 
-            pob_riesgo.append(SumCount(dq,'n_riesgo',x,'Sum'))
-        for x in class_riesgo: 
-            qm_riesgo.append(SumCount(dq,'n_riesgo',x,'Count'))
-        for i in pob_riesgo: 
-            percent_pob.append(round((i*100/total),1))
-                
         vh = SumCount(dq, "n_riesgo", 'Muy Alto', 'Sum')
         h = SumCount(dq, "n_riesgo", 'Alto', 'Sum')
         m = SumCount(dq, "n_riesgo", 'Medio', 'Sum')
         l = SumCount(dq, "n_riesgo", 'Bajo', 'Sum')
+        densidad_q1 = SumCount(dq, "q_densid", 'Q1', 'Sum')
+        densidad_q2 = SumCount(dq, "q_densid", 'Q2', 'Sum')
+        densidad_q3 = SumCount(dq, "q_densid", 'Q3', 'Sum')
+        densidad_q4 = SumCount(dq, "q_densid", 'Q4', 'Sum')
+        densidad_q5 = SumCount(dq, "q_densid", 'Q5', 'Sum')
         nombre = query.capitalize()
-        # table_riesgo = list(zip(class_riesgo, qm_riesgo, pob_riesgo, percent_pob,colores_riesgo))
-    
+        data_riesgo = getIndex(['Muy Alto', 'Alto', 'Medio', 'Bajo'], dq, 'n_riesgo', ["#dc3545c2", "#ff6a00c2", "#ffc107c2", "#198754c2", ])
+        data_densidad = getIndex(['Q1', 'Q2', 'Q3', 'Q4', 'Q5'], dq, 'q_densid', [
+                            '#fde725c2', '#5dc962c2', '#20908dc2', '#3a528bc2', '#440154c2'])
+
+        if len(dq) >= 1:
+            return render(request, r'dashboard/geoportal.html', {
+                'data': data,
+                'riesgo': data_riesgo['tabla'],
+                'total_manzana': data_riesgo['total_manzanas'],
+                'total_porcentaje': data_riesgo['total_porcentaje'],
+                'densidad': data_densidad['tabla'],
+                'vh': vh,
+                'h': h,
+                'm': m,
+                'l': l,
+                'densidad_q1': densidad_q1,
+                'densidad_q2': densidad_q2,
+                'densidad_q3': densidad_q3,
+                'densidad_q4': densidad_q4,
+                'densidad_q5': densidad_q5,
+
+                'total': total,
+
+                'nombre': nombre
+            })
+
+        else:
+            return render(request, r'shared/noexiste.html', {
+
+            })
+        pass
+
+
     vh = SumCount(dq, "n_riesgo", 'Muy Alto', 'Sum')
     h = SumCount(dq, "n_riesgo", 'Alto', 'Sum')
     m = SumCount(dq, "n_riesgo", 'Medio','Sum' )
     l = SumCount(dq, "n_riesgo", 'Bajo', 'Sum')
    
-    total = pobTotal(dq)
-
-
-
-    
-    total_manzana = sum(qm_riesgo)
-    total_porcentaje = round(sum(percent_pob),0)
-    
-
-
-    table_riesgo = list(zip(class_riesgo,qm_riesgo,pob_riesgo,percent_pob,colores_riesgo))
-    print(qm_riesgo, percent_pob)
+    total = pobTotal(dq)   
 
     if len(dq) >= 1:
         return render(request, r'dashboard/geoportal.html', {
             'data': data,
-            'info':table_riesgo,
+            'riesgo': data_riesgo['tabla'],
+            'total_manzana': data_riesgo['total_manzanas'],
+            'total_porcentaje': data_riesgo['total_porcentaje'],
+            'densidad': data_densidad['tabla'],
             'vh': vh,
             'h': h,
             'm': m,
             'l': l,
             'total': total,
-            'total_manzanas': total_manzana,
-            'total_porcentaje': total_porcentaje,
 
             'nombre': nombre
         })
@@ -200,8 +246,6 @@ def distIndicadores(request):
             'l': l,
             
             'total': total,
-            
-            ''
             'nombre': nombre
         })
     
