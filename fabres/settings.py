f8df36131359fa4117db26f961939bd711a35154
[Francisco Contreras] <franciscocontreras93@gmail.com>
2022-10-03 19:11:55 -0400

    fixed server heroku

--- fabres/settings.py
+++ fabres/settings.py
@@ -112,66 +112,66 @@ WSGI_APPLICATION = 'fabres.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 # CASO 1 PRODUCCION REMOTO
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'HOST': 'ec2-3-214-121-14.compute-1.amazonaws.com',
-        'PORT': '5432',
-        'NAME': 'd6t8mab450ah7e',
-        'USER': 'ubaxledfnaqxix',
-        'PASSWORD': '705a885bd9edeb6845b17e57f6f4eaaf6e4c19ad31b1636290f808f2fe7dae5a'
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.contrib.gis.db.backends.postgis',
+#         'HOST': 'ec2-3-214-121-14.compute-1.amazonaws.com',
+#         'PORT': '5432',
+#         'NAME': 'd6t8mab450ah7e',
+#         'USER': 'ubaxledfnaqxix',
+#         'PASSWORD': '705a885bd9edeb6845b17e57f6f4eaaf6e4c19ad31b1636290f808f2fe7dae5a'
+#     }
+# }
+
+if DEBUG == False and LOCAL == False: 
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.contrib.gis.db.backends.postgis',
+            'HOST': 'ec2-3-214-121-14.compute-1.amazonaws.com',
+            'PORT': '5432',
+            'NAME': 'd6t8mab450ah7e',
+            'USER': 'ubaxledfnaqxix',
+            'PASSWORD': '705a885bd9edeb6845b17e57f6f4eaaf6e4c19ad31b1636290f808f2fe7dae5a'
+        }
     }
-}
+# CASO 2 DEPURACION LOCAL
+elif DEBUG == True and LOCAL == True: 
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.contrib.gis.db.backends.postgis',
+            'HOST': 'localhost',
+            'PORT': '5432',
+            'NAME': 'geodjango',
+            'USER': 'postgres',
+            'PASSWORD': '23826405'
+        }
+    }
+# CASO 3 PRODUCCION LOCAL
+elif DEBUG == False and LOCAL == True:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.contrib.gis.db.backends.postgis',
+            'HOST': 'localhost',
+            'PORT': '5432',
+            'NAME': 'geodjango',
+            'USER': 'postgres',
+            'PASSWORD': '23826405'
+        }
 
-# if DEBUG == False and LOCAL == False: 
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.contrib.gis.db.backends.postgis',
-#             'HOST': 'ec2-3-214-121-14.compute-1.amazonaws.com',
-#             'PORT': '5432',
-#             'NAME': 'd6t8mab450ah7e',
-#             'USER': 'ubaxledfnaqxix',
-#             'PASSWORD': '705a885bd9edeb6845b17e57f6f4eaaf6e4c19ad31b1636290f808f2fe7dae5a'
-#         }
-#     }
-# # CASO 2 DEPURACION LOCAL
-# elif DEBUG == True and LOCAL == True: 
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.contrib.gis.db.backends.postgis',
-#             'HOST': 'localhost',
-#             'PORT': '5432',
-#             'NAME': 'geodjango',
-#             'USER': 'postgres',
-#             'PASSWORD': '23826405'
-#         }
-#     }
-# # CASO 3 PRODUCCION LOCAL
-# elif DEBUG == False and LOCAL == True:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.contrib.gis.db.backends.postgis',
-#             'HOST': 'localhost',
-#             'PORT': '5432',
-#             'NAME': 'geodjango',
-#             'USER': 'postgres',
-#             'PASSWORD': '23826405'
-#         }
+    }
+# CASO 4 DEPURACION REMOTA
+elif DEBUG == True and LOCAL == False:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.contrib.gis.db.backends.postgis',
+            'HOST': 'ec2-3-214-121-14.compute-1.amazonaws.com',
+            'PORT': '5432',
+            'NAME': 'd6t8mab450ah7e',
+            'USER': 'ubaxledfnaqxix',
+            'PASSWORD': '705a885bd9edeb6845b17e57f6f4eaaf6e4c19ad31b1636290f808f2fe7dae5a'
+        }
 
-#     }
-# # CASO 4 DEPURACION REMOTA
-# elif DEBUG == True and LOCAL == False:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.contrib.gis.db.backends.postgis',
-#             'HOST': 'ec2-3-214-121-14.compute-1.amazonaws.com',
-#             'PORT': '5432',
-#             'NAME': 'd6t8mab450ah7e',
-#             'USER': 'ubaxledfnaqxix',
-#             'PASSWORD': '705a885bd9edeb6845b17e57f6f4eaaf6e4c19ad31b1636290f808f2fe7dae5a'
-#         }
-
-#     }
+    }
 
 
 
